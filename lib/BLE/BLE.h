30923cf5ee867f1d1d89bafca6dfd6dee0fd6473
joaogabrielsantosD <joao.donato@ufpe.br>
2024-04-15 17:39:12 -0300

    BLE without message.h depedence

--- lib/BLE/BLE.h
+++ lib/BLE/BLE.h
@@ -3,32 +3,27 @@
 
 #include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEServer.h>
 #include <BLEUtils.h>
-#include <BLE2902.h>
-#include "message.h"
+#include <BLEServer.h>
 
-//#define SERVICE_UUID            "ab0828b1-198e-4351-b779-901fa0e0371e"
-//#define CHARACTERISTIC_UUID_RX  "4ac8a682-9736-4e5d-932b-e9b31405049c"
-//#define CHARACTERISTIC_UUID_TX  "0972EF8C-7613-4075-AD52-756F33D4DA91"
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID   "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-#define MAX_BLE_LENGTH 64
+#define MAX_BLE_LENGTH ESP_GATT_MAX_ATTR_LEN
 #define MAX_BLE_DELAY  90
 
 void setup_BLE(void);
-void BLE_connected(BLEmsg_t Q);
-void BLE_Sender(void* T, int len);
+int BLE_connected(void);
+void BLE_Sender(void* T, uint32_t len);
 
-//callback para eventos das características
+// Callback para eventos das características
 class CharacteristicCallbacks: public BLECharacteristicCallbacks 
 {
     public:
         void onWrite(BLECharacteristic* characteristic); 
 };
 
-//callback para receber os eventos de conexão de dispositivos
+// Callback para receber os eventos de conexão de dispositivos
 class ServerCallbacks: public BLEServerCallbacks 
 {
     public: 
@@ -36,6 +31,5 @@ class ServerCallbacks: public BLEServerCallbacks
 
         void onDisconnect(BLEServer* pServer);
 };
-
 
 #endif
