a7838a238eef4edb403dde88fd446ba10c964641
joaogabrielsantosD <joao.donato@ufpe.br>
2024-04-15 17:43:28 -0300

    Add Circular buffer lib in local file

--- lib/StateMachine/StateMachine.h
+++ lib/StateMachine/StateMachine.h
@@ -2,10 +2,11 @@
 #define STATEMACHINE_H
 
 #include <Arduino.h>
-#include "CAN_PIDs.h"
+#include <CircularBuffer.hpp>
 #include "can_defs.h"
+#include "CAN_PIDs.h"
 
-/* CicularBuffer Defs: */
+/* CicularBuffer Defs */
 typedef enum {IDLE_ST, DistanceTraveled_ST, EngineRPM_ST, VehicleSpeed_ST, FuelLevel_ST, EngineCoolant_ST} state_t;
 
 state_t CircularBuffer_state(void);
