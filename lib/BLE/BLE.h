47fd4170bc76069f363127ec64c4341340333501
joaogabrielsantosD <joao.donato@ufpe.br>
2024-04-25 22:55:39 -0300

    Remove useless lib

--- lib/BLE/BLE.h
+++ lib/BLE/BLE.h
@@ -3,8 +3,6 @@
 
 #include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
 
 #define SERVICE_UUID          "acc1f4ef-4fcf-4f90-882e-0a666da9f321"
 #define CHARACTERISTIC_UUID   "50e6cbc6-5aff-4423-974c-0e27959453c3"
