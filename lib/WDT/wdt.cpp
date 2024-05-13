4d00c9ddc501b553ec2ba5d1b93c0b4925fb2abc
joaogabrielsantosD <joao.donato@ufpe.br>
2024-05-13 10:08:29 -0300

    Disable the WDT before set the new time

--- lib/WDT/wdt.cpp
+++ lib/WDT/wdt.cpp
@@ -3,12 +3,23 @@
 // example from: https://www.youtube.com/watch?v=yOwg1EMby2g&t=240s
 void set_wdt_timer()
 {
-  rtc_wdt_protect_off(); // Disable RTC WDT write protection
-  rtc_wdt_set_stage(RTC_WDT_STAGE0, RTC_WDT_STAGE_ACTION_RESET_RTC); 
-  // Set stage 0 to trigger a system reset after 9000ms
-  rtc_wdt_set_time(RTC_WDT_STAGE0, 9000); 
-  rtc_wdt_enable();
-  rtc_wdt_protect_on();
+  static bool first_exec = true;
+
+  if(first_exec)
+  {
+    rtc_wdt_disable();
+    first_exec = false;
+  }
+
+  else
+  {
+    rtc_wdt_protect_off(); // Disable RTC WDT write protection
+    rtc_wdt_set_stage(RTC_WDT_STAGE0, RTC_WDT_STAGE_ACTION_RESET_RTC); 
+    // Set stage 0 to trigger a system reset after 9000ms
+    rtc_wdt_set_time(RTC_WDT_STAGE0, 9000); 
+    rtc_wdt_enable();
+    rtc_wdt_protect_on();
+  }
 }
 
 void reset_rtc_wdt()
