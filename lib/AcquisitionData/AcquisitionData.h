a0549505a638958b0e1375f79333ddc67e6d7d39
joaogabrielsantosD <joao.donato@ufpe.br>
2024-05-13 10:11:12 -0300

    Remove the bit analyze functions

--- lib/AcquisitionData/AcquisitionData.h
+++ lib/AcquisitionData/AcquisitionData.h
@@ -2,18 +2,14 @@
 #define ACQUISITIONDATA_H
 
 #include <Arduino.h>
-#include <CircularBuffer.hpp>
-#include "can_defs.h"
+#include "StateMachine.h"
 
-typedef struct 
-{
+typedef struct {
     float a;
 } wifi_packet_t;
 
 /* Circular Buffer Functions */
-int CircularBuffer_state(void);
-bool insert(int ST);
-void debug_print(unsigned char* message);
+bool Check_Current_State_Machine(void);
 
 /* Accelerometer && GPS functions */
 // acc function
@@ -21,9 +17,5 @@ void debug_print(unsigned char* message);
 
 /* CAN Acquisition functions */
 void MsgRec_Treatment(void);
-void Storage_PIDenable_bit(unsigned char* bit_data, int8_t position);
-void Convert_Dec2Bin(void);
-int Check_bin_for_state(int pid_order);
-int Verify_odometer_exist(void);
 
 #endif
