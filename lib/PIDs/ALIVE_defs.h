dbeabffb2d9237d36d5add479d51918caf043b80
joaogabrielsantosD <joao.donato@ufpe.br>
2024-06-03 16:32:49 -0300

    Add the pin of debug led

--- lib/PIDs/ALIVE_defs.h
+++ lib/PIDs/ALIVE_defs.h
@@ -46,8 +46,8 @@
 #define PID_to_index_5      16
 
 /*====================== HARDWARE DEFINITIONS ============================ */
-//#define CAN_DEBUG_LED GPIO_NUM_25 // !< Pin not confirm
-//#define BLE_DEBUG_LED GPIO_NUM_26 // !< Pin not confirm
+#define CAN_DEBUG_LED GPIO_NUM_25
+#define BLE_DEBUG_LED GPIO_NUM_26
 #define SPI_CS_PIN    GPIO_NUM_4  // Pin CS to the MCP2515 module
 #define CAN_INT_PIN   GPIO_NUM_27 // Pin used to generate the interrupt by the MCP2515 module
 
