c9d3934129b8f060abf3bd5794b24799b8d40c3b
joaogabrielsantosD <joao.donato@ufpe.br>
2024-08-17 23:13:10 -0300

    FEAT: Remove make_dtc code function

--- lib/AcquisitionData/AcquisitionData.h
+++ lib/AcquisitionData/AcquisitionData.h
@@ -6,12 +6,7 @@
 #include <pthread.h>
 #include "StateMachine.h"
 #include "packets.h"
-
-#define verify_char(c) (c == 10 ? 'A' : c == 11 ? 'B' \
-                                      : c == 12 ? 'C' \
-                                      : c == 13 ? 'D' \
-                                      : c == 14 ? 'E' \
-                                      : c == 15 ? 'F' : 'Z') // If c < 10, send the character 'Z' as an error flag
+#include "Messages.hpp"
 
 typedef void *ThreadHandle_t;
 
@@ -24,7 +19,6 @@ ThreadHandle_t gps_acq_function(void *arg);
 
 /* CAN Acquisition functions */
 void Handling_CAN_msg(void);
-String make_DTC_code(uint8_t first_msg, uint8_t second_msg);
 void cleanDTC(void);
 
 /* Packet Message Functions */
