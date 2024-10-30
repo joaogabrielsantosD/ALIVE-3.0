a117b090c0648a044bab048b8f992993b790f196
joaogabrielsantosD <joao.donato@ufpe.br>
2024-10-30 15:42:13 -0300

    Update define

--- lib/BLE_transmitter/BLE.h
+++ lib/BLE_transmitter/BLE.h
@@ -3,14 +3,13 @@
 
 #include <BLEDevice.h>
 #include <ArduinoJson.h>
-#include "CanFunctions.h"
-#include "tickerISR.h"
+#include "CircularBufferState.h"
 
 #define SERVICE_UUID        "acc1f4ef-4fcf-4f90-882e-0a666da9f321"
 #define CHARACTERISTIC_UUID "50e6cbc6-5aff-4423-974c-0e27959453c3"
 
-#define MAX_BLE_LENGTH       ESP_GATT_MAX_ATTR_LEN // 600 bytes
-#define MAX_BLE_DELAY        90
+#define MAX_BLE_LENGTH       512
+#define MAX_BLE_DELAY        100 // ms
 #define DOC_SIZE_JSON        400
 
 void Init_BLE_Server(void);
@@ -25,11 +24,5 @@ class ServerCallbacks : public BLEServerCallbacks
 
         void onDisconnect(BLEServer *pServer);
 };
-
-// class CharacteristicCallbacks : public BLECharacteristicCallbacks
-// {
-//     public:
-//         void onWrite(BLECharacteristic *SenderCharacteristic);
-// };
 
 #endif
