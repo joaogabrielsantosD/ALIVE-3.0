37c88efcb063883a4de1f4169c8b29f8f63e4060
joaogabrielsantosD <joao.donato@ufpe.br>
2024-11-07 11:58:48 -0300

    Change property to READ

--- lib/BLE_transmitter/BLE.cpp
+++ lib/BLE_transmitter/BLE.cpp
@@ -30,7 +30,7 @@ void Init_BLE_Server()
     pCharacteristic = pService->createCharacteristic( \
         CHARACTERISTIC_UUID,                          \
         BLECharacteristic::PROPERTY_NOTIFY |          \
-        BLECharacteristic::PROPERTY_WRITE             \
+        BLECharacteristic::PROPERTY_READ              \
         );
 
     // Create a BLE Descriptor
